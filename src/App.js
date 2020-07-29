1995f40633326155bcfcd71819455f2d4a02da71
Luis <luis.gilgb@gmail.com>
2020-07-29 18:49:18 +0200

    Creating form to add points

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
 import React from "react";
 import Path from "./path/Path";
 import { EXAMPLE_POINTS } from "./data";
+import { Box, Flex } from "rebass";
+import { Label, Input } from "@rebass/forms";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <svg width="100%" height={600}>
-        <Path
-          points={EXAMPLE_POINTS}
-          stroke="black"
-          strokeWidth={1}
-          fill="red"
-          closePath
-        />
-      </svg>
+      <Flex>
+        <Box as="form" width={240} p={1}>
+          <Flex m={2}>
+            <Label
+              flex={1}
+              px={3}
+              justifyContent="flex-end"
+              alignItems="center"
+              htmlFor="x"
+            >
+              x
+            </Label>
+            <Input
+              width={120}
+              name="x"
+              type="number"
+              min={0}
+              defaultValue={0}
+            />
+          </Flex>
+          <Flex m={2}>
+            <Label
+              flex={1}
+              px={3}
+              justifyContent="flex-end"
+              alignItems="center"
+              htmlFor="y"
+            >
+              y
+            </Label>
+            <Input
+              width={120}
+              name="y"
+              type="number"
+              min={0}
+              defaultValue={0}
+            />
+          </Flex>
+        </Box>
+        <Box>
+          <svg width={600} height={600}>
+            <Path
+              points={EXAMPLE_POINTS}
+              stroke="black"
+              strokeWidth={1}
+              fill="red"
+              closePath
+            />
+          </svg>
+        </Box>
+      </Flex>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { ThemeProvider } from "emotion-theming";
+import theme from "@rebass/preset";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   rootElement
 );
